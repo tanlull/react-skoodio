c9a361ccdcaf1e9593e5e431b58ba61a864538d5
Tanya Sattaya-aphitan <tanlull@gmail.com>
2021-03-25 12:29:24 +0700

    create board

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react'
+
 function Cell(props) {
+  const [number, setNumber] = useState(1)
+
   return (
-    <div className="cell">{props.number}</div>
+    <div onClick={(e => {
+      setNumber(number + 1)
+    })}
+      className={`cell ${props.isInitial ? 'initial' : ''}`
+      }
+    > { props.number != 0 && props.number}</div >
+  );
+}
+
+function Board(props) {
+  const [board, setBoard] = useState([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]);
+  const [initial, setInitial] = useState([[true, false, true, false], [true, false, true, false], [true, false, true, false], [true, false, false, false]]);
+
+  return (
+    <div className="board">
+      {
+        board.map((row, i) => row.map((number, j) => <Cell key={`cell-${i}-${j}`} number={number} isInitial={initial[i][j]} />))
+      }
+
+    </div >
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <div className="board">
-        <Cell number={3} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-        <Cell number={4} />
-      </div >
+      <Board />
     </div >
   );
 }
