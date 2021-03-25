814f8c157820571a72ba05dc71b7990d02bbe5be
Tanya Sattaya-aphitan <tanlull@gmail.com>
2021-03-25 11:42:33 +0700

    Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Cell(props) {
+  return (
+    <div className="cell">{props.number}</div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="board">
+        <Cell number={3} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+        <Cell number={4} />
+      </div >
+    </div >
   );
 }
 
